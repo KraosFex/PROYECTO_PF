c6ac25f3729880e0df50d70ba49029fddef0e757
cesar <cesar.bfernandez@gmail.com>
2022-06-06 17:05:11 -0400

    agregado el JWT

--- api-app/src/controllers/controlllerAuth.js
+++ api-app/src/controllers/controlllerAuth.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const User = require('../model/modelUser.js')
 
@@ -9,7 +10,9 @@ const authUser = async (req, res) => {
         if (!user) return res.send({ info: 'Datos invalidos user o pass not found' })
         const validPass = await bcrypt.compareSync(password, user.password)
         if (!validPass) return res.send({ info: 'Datos invalidos user o pass not found' })
-        res.send({ info: 'Usuario autenticado', user })
+        const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, { expiresIn: '1h' })
+
+        res.send({ info: 'Usuario autenticado', token })
     } catch (err) {
         res.send({ info: 'Error al autenticar el usuario', err })
         console.log(err)
