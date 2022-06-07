f210a3df492bd05884e3091cc683f9988c27b14f
agustin panizza <agustin_panizza@hotmail.com>
2022-06-06 22:01:42 -0300

    front

---
+++ client-app/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import NavBar from "./Components/navbar/navBar";
+import Home from "./pages/home";
+import NavBarUser from "./Components/navbar/navBarUser";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <NavBar />
+      <NavBarUser />
+    </div>
+  );
+}
+
+export default App;
