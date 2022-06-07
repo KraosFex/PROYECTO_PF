4892f005a442f607204420865552e9448d000843
Valentina <vcolmenares>
2022-06-07 16:53:49 -0300

    Add Register and Routes (./client/scr/App-Components/Register)

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -1,17 +1,17 @@
 
+import { Route, Routes } from "react-router-dom";
+import Register from "./components/Register/Register";
 import "./index.css";
 import Home from "./pages/home";
-import NavBar from "./Components/navbar/navBar";
-import NavBarUser from "./Components/navbar/navBarUser";
-
-
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <NavBar />
-      <NavBarUser />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path ="/Register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
