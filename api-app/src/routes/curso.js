5e6e18c4323996ed40b3154aecc3979df6828385
cesar <cesar.bfernandez@gmail.com>
2022-06-07 20:11:22 -0400

    mejora(auth y proteccion de rura creacion curso)

--- api-app/src/routes/curso.js
+++ api-app/src/routes/curso.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const router = Router();
-const {getCursos, getCursoId, createCurso} = require('../controllers/controllerCursos.js')
+const { getCursos, getCursoId, createCurso } = require('../controllers/controllerCursos.js')
+const {validateAuthAndAnAdmin} = require('../utils/validate.js')
 
 router.get('/', getCursos)
 router.get('/:id', getCursoId)
-router.post('/', createCurso)
+router.post('/', validateAuthAndAnAdmin, createCurso)
 
 module.exports = router;
