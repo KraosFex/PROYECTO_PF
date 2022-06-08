78eb999f42a5503ca84c35a267291f7de2bd1ca2
agustin panizza <agustin_panizza@hotmail.com>
2022-06-07 23:00:19 -0300

    Home + cambio de tema

--- api-app/src/model/modelCurso.js
+++ api-app/src/model/modelCurso.js
@@ -1,34 +1,47 @@
-const { Schema, model } = require("mongoose");
+const {Schema, model} = require('mongoose');
 
-const courseSchema = new Schema(
-  {
+const courseSchema = new Schema({
     titulo: {
-      type: String,
-      required: true,
+        type: String,
+        required: true
     },
     descripcion: {
-      type: String,
-      required: true,
+        type: String,
+        required: true
     },
     calificacion: {
-      type: Number,
-      required: true,
+        type: Number,
+        required: true
     },
     imagen: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     userInscrip: {
-      type: Number,
+        type: Number,
     },
     clases: {
-      type: [String],
-    },
-  },
-  {
+        type: [String],
+    }
+}, {
     timestamps: true,
     versionKey: false,
-  }
-);
+})
 
-module.exports = model("Course", courseSchema);
+
+module.exports = model('Course', courseSchema)
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    classes: {
+        type: Array,
+        required: true,
+        validate: c => c.length > 0
+    }
+
+module.exports = model('Course', courseSchema)
--- api-app/src/routes/user.js
+++ api-app/src/routes/user.js
@@ -1,14 +1,13 @@
-const { Router } = require("express");
+const {Router} = require('express');
 const router = Router();
-const {
-  getUsers,
-  createUser,
-  getUserById,
-} = require("../controllers/controllerUser.js");
-const { validateAuth } = require("../utils/validate.js");
+const { getUsers, createUser, getUserById } = require('../controllers/controllerUser.js')
+const { validateAuth } = require('../utils/validate.js')
 
-router.get("/", getUsers);
-router.post("/", createUser);
-router.get("/:id", validateAuth, getUserById);
+
+router.get('/', validateAuth, getUsers)
+router.post('/', createUser)
+router.get('/:id', validateAuth, getUserById)
+
+
 
 module.exports = router;
