e42432eb10f9e267fcd691cf95b160552b636be2
homero <homerogazze1015@gmail.com>
2022-06-10 10:22:53 -0300

    Fix(Removing some duplicated folders)

--- api-app/api-app/src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express');
-const router = Router();
-const { authUser } = require('../controllers/controlllerAuth')
-
-
-router.get('/', authUser)
-
-
-module.exports = router;
