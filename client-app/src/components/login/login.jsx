4ecb182e960a79f66c39ff718f210120a7e69857
homero <homerogazze1015@gmail.com>
2022-06-09 21:06:54 -0300

    PullRequest(Fixing bug on LogIn component, in Validator function)

--- client-app/src/components/login/login.jsx
+++ client-app/src/components/login/login.jsx
@@ -15,7 +15,7 @@ const validator = (input) => {
 
   if(!input.password){
     error.password = "password is required";
-  } else if (*!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(input.password)){
+  } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(input.password)){
     error.password = "Minimum 8 characters, at least one uppercase letter, one lowercase letter and one number";
   }
 
