9bc4f0f57f9aed105e70d401251a06124c9b5204
ChampiMagic <97173716+ChampiMagic@users.noreply.github.com>
2022-06-12 17:08:24 -0300

    Update Store.js

--- client-app/redux/store/Store.js
+++ client-app/redux/store/Store.js
@@ -1,8 +1,6 @@
 // Store app
 import { legacy_createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from '../reducer/index.js';
-
 import rootReducer from "../reducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
