0d2008fbb0a120a74c6ff53b368480e651928f0e
KraosFex <ferreirajohan14253@gmail.com>
2022-06-12 00:18:47 -0400

    update(client-app/src/components/logout) update the component

--- client-app/src/components/logout/logout.jsx
+++ client-app/src/components/logout/logout.jsx
@@ -1,7 +1,16 @@
-const logout = () => {
+import { NavLink } from 'react-router-dom'
+import { useDispatch } from "react-redux"
+import { logout } from "../../../redux/actions";
+
+const Logout = () => {
+
+    const dispatch = useDispatch()
+
+    const handleClick = () => dispatch(logout())
+
     return(
-        <button > logout </button>
+        <NavLink to='/'><button onClick={() => handleClick }> logout </button></NavLink>
     )
 }
 
-export default logout;
+export default Logout;
