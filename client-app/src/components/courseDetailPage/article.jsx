e8f811bc7dacc7c93181c6530e0e19382e600556
Valentina <vcolmenares>
2022-06-15 14:55:42 -0300

    Fix propiedas del Curso .src/Detail/article&&card/card

--- client-app/src/components/courseDetailPage/article.jsx
+++ client-app/src/components/courseDetailPage/article.jsx
@@ -5,9 +5,10 @@ import { useParams } from "react-router-dom";
 import Card from "./card/card";
 import style from "./course/course.module.css"
 
-let clase = { title: "Clase", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.    Mollitia, deserunt rerum. Aspernatur, quos.", temas: ["Java", "script", "Front", "Back"], isCompleted: false }
+let lessons = { titulo: "Clase", descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.    Mollitia, deserunt rerum. Aspernatur, quos." , isCompleted: false }
 
 let c = 3
+
 function Change(clase, index, set) {
     if (clase === "next") { set([index, index + c]) }
     if (clase === "prev") { set([index - c, index]) }
@@ -17,14 +18,14 @@ export default function CardD() {
 
     let [index, setIndex] = useState([0, c])
     let id = useParams().id;
-    const { CurseDetail } = useSelector((state) => state);
+    const { CurseDetail, user } = useSelector((state) => state);
 
-    let CursoBase = { id, titulo: "Curso " + id, calificacion: 2000, imagen: "http://www.venturapp.com/wp-content/uploads/2016/03/html_elementos_etiquetas_usadas-680x350.jpg", userIncript: 4567, description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.    Mollitia, deserunt rerum. Aspernatur, quos.    Quasi dolores earum, non minus ipsa iure sunt,    odio distinctio assumenda ullam sit ea perferendis quaerat facilis?Lorem ipsum dolor sit amet, consectetur adipisicing elit.    Mollitia, deserunt rerum. Aspernatur, quos.    Quasi dolores earum, non minus ipsa iure sunt,    odio distinctio assumenda ullam sit ea perferendis quaerat facilis", clases: [{ ...clase, id: 1, isCompleted: true }, { ...clase, id: 2, isCompleted: true }, { ...clase, id: 3, isCompleted: true }, { ...clase, id: 4 }, { ...clase, id: 5 }, { ...clase, id: 6 }, { ...clase, id: 7 }] }
+    let CursoBase = { id, titulo: "Curso " + id, calificacion: 2000, imagen: "http://www.venturapp.com/wp-content/uploads/2016/03/html_elementos_etiquetas_usadas-680x350.jpg", userIncript: 4567, description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.    Mollitia, deserunt rerum. Aspernatur, quos.    Quasi dolores earum, non minus ipsa iure sunt,    odio distinctio assumenda ullam sit ea perferendis quaerat facilis?Lorem ipsum dolor sit amet, consectetur adipisicing elit.    Mollitia, deserunt rerum. Aspernatur, quos.    Quasi dolores earum, non minus ipsa iure sunt,    odio distinctio assumenda ullam sit ea perferendis quaerat facilis", lessons: [{ ...lessons, num: 1, isCompleted: true }, { ...lessons, num: 2, isCompleted: true }, { ...lessons, num: 3, isCompleted: true }, { ...lessons, num: 4 }, { ...lessons, num: 5 }, { ...lessons, num: 6 }, { ...lessons, num: 7 }] }
 
     let Curso = CursoBase;
-    console.log(Curso)
-    let MaxClase = Curso.clases.length
-    let list = Curso.clases.slice(index[0], index[1])
+    let MaxClase = Curso.lessons.length
+    let list = Curso.lessons.slice(index[0], index[1])
+    let progress = user.courses? user.courses : CursoBase.lessons.map(e=>{return{course: e, isFavorite:false}})
     return (
         <div className={style.Container}>
             <div className={style.sup}>
@@ -44,9 +45,9 @@ export default function CardD() {
             </div>
             <h4 className={style.h4}>Progress:</h4>
             <div className={style.progreso}>
-                {Curso.clases.map((e) => <div className={style.ClasP}>{e.isCompleted ? <input key={e.id} checked type="radio" readOnly/>: <input key={e.id} type="radio" disabled/>}{e.title}</div>)}
-                {Curso.clases.map((e) => <div className={style.ClasP}>{e.isCompleted ? <input key={e.id} checked type="radio" readOnly/>: <input key={e.id} type="radio" disabled/>}{e.title}</div>)}
-                {Curso.clases.map((e) => <div className={style.ClasP}>{e.isCompleted ? <input key={e.id} checked type="radio" readOnly/>: <input key={e.id} type="radio" disabled/>}{e.title}</div>)}
+                {progress.map((e) => <div className={style.ClasP}>{e.course.isCompleted ? <input key={e.course.num} checked type="radio" readOnly/>: <input key={e.num} type="radio" disabled/>}{e.course.titulo}</div>)}
+                {progress.map((e) => <div className={style.ClasP}>{e.course.isCompleted ? <input key={e.course.num} checked type="radio" readOnly/>: <input key={e.num} type="radio" disabled/>}{e.course.titulo}</div>)}
+                {progress.map((e) => <div className={style.ClasP}>{e.course.isCompleted ? <input key={e.course.num} checked type="radio" readOnly/>: <input key={e.num} type="radio" disabled/>}{e.course.titulo}</div>)}
             </div>
 
         </div>
