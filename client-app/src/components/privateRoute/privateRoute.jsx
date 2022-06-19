6f178eff2c11e16ce6ee9236304c1b2330aaa2ea
Johan Ferreira <86781666+KraosFex@users.noreply.github.com>
2022-06-19 13:24:31 -0400

    Update privateRoute.jsx

--- client-app/src/components/privateRoute/privateRoute.jsx
+++ client-app/src/components/privateRoute/privateRoute.jsx
@@ -1,15 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
 
-<<<<<<< HEAD
 const PrivateRoute = ({ isLogged }) => {
   return isLogged ? <Outlet /> : <Navigate to="/login" />;
 };
-=======
+
 const PrivateRoute = ( {isLogged} ) => {
-
     return isLogged? <Outlet /> : <Navigate to='/login' />
-
 }
->>>>>>> 1be860b2a19cc436c0e18551cdb7b03808c791cf
 
 export default PrivateRoute;
