84dad26ab168b993bb8568a503466f8143873531
ChampiMagic <97173716+ChampiMagic@users.noreply.github.com>
2022-06-19 14:38:32 -0300

    Update login.jsx
    Fixing bad merging bugas

--- client-app/src/components/login/login.jsx
+++ client-app/src/components/login/login.jsx
@@ -16,26 +16,6 @@ function Login() {
   const [error, setError] = useState({});
   const [logError, setLogError] = useState({});
   const [forgotPopUp, setForgotPopUp] = useState(false);
-=======
-import axios from 'axios'
-import jwt_decode from 'jwt-decode'
-import React, { useEffect, useState } from 'react'
-import style from './login.module.css'
-import { validation } from '../../../redux/actions/index';
-import { useDispatch, useSelector } from 'react-redux';
-import ForgotPopUp from "./popUp/forgotPasswordPopUp.jsx";
-import validator from "../../utils/validator.js";
-import { Navigate } from 'react-router-dom'
-
-function Login () {
-
-  const dispatch = useDispatch();
-
-  const [input, setInput] = useState({})
-  const [error, setError] = useState({})
-  const [logError, setLogError] = useState({})
-  const [forgotPopUp, setForgotPopUp] = useState(false)
->>>>>>> 1be860b2a19cc436c0e18551cdb7b03808c791cf
 
   const workOnChange = (event) => {
     setInput({
@@ -43,21 +23,12 @@ function Login () {
       [event.target.name]: event.target.value,
     });
 
-<<<<<<< HEAD
-    setError(
-      validator("login", {
-        ...input,
-        [event.target.name]: event.target.value,
-      })
-    );
-  };
-=======
+
     setError(validator("login", {
       ...input,
       [event.target.name]: event.target.value
     }))
   }
->>>>>>> 1be860b2a19cc436c0e18551cdb7b03808c791cf
 
   const handleSubmit = async (event) => {
     function isObjectEmpty(obj) {
@@ -70,23 +41,15 @@ function Login () {
     if (!isObjectEmpty(error) || isObjectEmpty(input)) {
       event.preventDefault();
 
-<<<<<<< HEAD
       setError(
         validator("login", {
           ...input,
           [event.target.name]: event.target.value,
         })
       );
-=======
-      setError(validator("login", {
-        ...input,
-        [event.target.name]: event.target.value
-      }))
->>>>>>> 1be860b2a19cc436c0e18551cdb7b03808c791cf
     } else {
       event.preventDefault();
 
-<<<<<<< HEAD
       const response = await dispatch(
         validation({ email: input.email, password: input.password })
       );
@@ -99,18 +62,7 @@ function Login () {
       }
     }
   };
-=======
-        const response = await dispatch(validation({email: input.email, password: input.password}))
-        if(response.success) {
-            localStorage.setItem("authToken", response.token)
-            setLogError({});
-            <Navigate to='/home' />
-          } else {
-            setLogError({err: response.info});
-          }
-    }
-  }
->>>>>>> 1be860b2a19cc436c0e18551cdb7b03808c791cf
+
 
   const handleCallBackResponse = async (response) => {
     const userObject = jwt_decode(response.credential);
@@ -118,15 +70,9 @@ function Login () {
     if (userObject.email_verified) {
       try {
         setLogError({});
-<<<<<<< HEAD
         <Navigate to="/home" />;
       } catch (err) {
         setLogError({ err: err });
-=======
-        <Navigate to='/home' />
-      } catch (err) {
-        setLogError({err: err});
->>>>>>> 1be860b2a19cc436c0e18551cdb7b03808c791cf
       }
     }
   };
@@ -156,7 +102,6 @@ function Login () {
   }, []);
 
   return (
-<<<<<<< HEAD
     <body className={style.body}>
       <div className={style.HeightContainer}>
         <div className={style.parentContainer}>
@@ -217,58 +162,13 @@ function Login () {
             <div className={style.googleInput}>
               <div id="signInDiv" />
             </div>
-=======
-    <div className={style.HeightContainer}>
-      <div className={style.parentContainer}>
-        <div className={style.childContainer}>
-          <h3>Lorem ipsum dolor sit amet, vivamus eu augue erat. Donec rutrum suscipit muris at blandit. Vestibulum loborti varius feugiat. Quisque at varius nibh</h3>
-          <div className={style.imgContainer}>
-            <img src='../../img/rocketLogin.png' />
-          </div>
-        </div>
-        <div className={style.childContainer}>
-          <h1>Sign In</h1>
-          {logError.err && <label className={style.logError}>{logError.err}</label>}
-          <form className={style.form} onChange={(e) => workOnChange(e)} onSubmit={(e) => handleSubmit(e)}>
-            <div className={style.input}>
-              <label className={style.title}>EMAIL</label>
-              <input type='text' placeholder='example@gmail.com' name='email' />
-              {error.email && <label className={style.error}>{error.email}</label>}
-            </div>
-            <div className={style.input}>
-              <label className={style.title}>PASSWORD</label>
-              <input type='password' placeholder='password' name='password' />
-              {error.password && <label className={style.error}>{error.password}</label>}
-            </div>
-            <button type='submit' className={style.button2}>
-              <span />
-              <span />
-              <span />
-              <span />
-              SIGN IN
-            </button>
-          </form>
-          {/*PopUp Trigger*/}
-          <a onClick={() => popUpFunction(true)}>Forgot password?</a>
-          <label className={style.googleLabel}>Or sign in With your google account</label>
-          <div className={style.googleInput}>
-            <div id='signInDiv' />
->>>>>>> 1be860b2a19cc436c0e18551cdb7b03808c791cf
           </div>
         </div>
         {/*Codition open popUp or Close popUp*/}
         {forgotPopUp ? <ForgotPopUp popUpFunction={popUpFunction} /> : null};
       </div>
-<<<<<<< HEAD
     </body>
   );
-=======
-      {/*Codition open popUp or Close popUp*/}
-      {forgotPopUp ? <ForgotPopUp popUpFunction={popUpFunction}/> : null};
-    </div>
-
-  )
->>>>>>> 1be860b2a19cc436c0e18551cdb7b03808c791cf
 }
 
 export default Login;
