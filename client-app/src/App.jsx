20857f54cec137c4979f6d11e8396b2b1dd42be0
ChampiMagic <97173716+ChampiMagic@users.noreply.github.com>
2022-06-19 12:56:04 -0300

    Update App.jsx
    Fixing bad merging Bugs.

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -15,12 +15,9 @@ import Perfil from "./components/perfil/perfil";
 import PrivateRoute from "./components/privateRoute/privateRoute";
 import PrivateAdminRoute from "./components/privateRoute/PrivateAdminRoute";
 import CourseDetailPage from "./components/courseDetailPage/course/course";
-<<<<<<< HEAD
-=======
 import LessonPage from "./components/lessonPage/lessonPage";
 import UsersPage from "./components/adminPages/usersPage/usersPage";
 
->>>>>>> 1be860b2a19cc436c0e18551cdb7b03808c791cf
 
 function App() {
   const theme = useSelector((store) => store.theme);
@@ -50,14 +47,10 @@ function App() {
         <Route element={<AppLayout />}>
           <Route path="/home" element={<Home theme={theme} />} />
           <Route path="/courses" element={<Courses />}></Route>
-          <Route
-            path="/course/:id"
-            element={<CourseDetailPage theme={theme} />}
-          />
-          <Route path="/perfil1" element={<Perfil theme={theme} />} />
+          <Route path="/course/:id" element={<CourseDetailPage theme={theme} />} />
           <Route element={<PrivateRoute isLogged={isLogged} />}>
             <Route path="/perfil" element={<Perfil />} />
-            <Route path="/course/:idCourse/:idLesson" element={<LessonPage />}/>
+            <Route path="/course/:idCourse/:idLesson" element={<LessonPage />} />
           </Route>
 
           <Route element={<PrivateAdminRoute isAdmin={user.isAdmin}/>}>
