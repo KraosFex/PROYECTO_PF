f008400d084168930914f3044991af681c812046
Samir José <samirjose675@gmail.com>
2022-06-21 17:35:25 -0300

    Add router.get('/topten')

--- api-app/src/routes/public/user.js
+++ api-app/src/routes/public/user.js
@@ -1,7 +1,8 @@
 const { Router } = require('express')
 const router = Router()
-const { getUserById } = require('../../controllers/controllerUser.js')
+const { getUserById, topTen } = require('../../controllers/controllerUser.js')
 
+router.get('/topten', topTen)
 router.get('/:id', getUserById)
 
 module.exports = router
