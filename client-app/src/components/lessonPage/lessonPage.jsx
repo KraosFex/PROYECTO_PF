c91709af4b6493f4c33e55f24ca323bfac3a8cf2
KraosFex <ferreirajohan14253@gmail.com>
2022-06-22 11:06:10 -0400

    added new libreri

--- client-app/src/components/lessonPage/lessonPage.jsx
+++ client-app/src/components/lessonPage/lessonPage.jsx
@@ -1,42 +1,14 @@
-// libraries
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { useDispatch } from 'react-redux';
-
-// redux actions 
-import { getLesson } from '../../../redux/actions';
-
 // style
 import style from './lessonPage.module.css';
 
 function LessonPage() {
 
-const dispatch = useDispatch();
-
-const { idCourse, idLesson} = useParams();
-
-const [lesson, setLesson] = useState({});
-
-useEffect(() => {
-
-  const axiosData = async () => {
-      try {
-        const lessonData = await dispatch(getLesson(idCourse, idLesson));
-        setLesson(lesson);
-      } catch(err) {
-        alert("algo paso pa");
-      }
-  }
-
-axiosData();
-}, [dispatch]);
-
   return (
     <div className={style.highContainer}>
       {/*ESTA ES TODA LA DATA QUE TRAE EL COMPONENETE DE MOMENTO*/}
       <div className={style.infoContainer}>
-        <h1 className={style.title}>{lesson.title}</h1>
-        <h4 className={style.description}>{lesson.descripcion}</h4>
+        <h1 className={style.title}>Hello orld</h1>
+        <h4 className={style.description}>Una description</h4>
       </div>
       {/*ACA IRIA EL COMPONENTE QUE TIENE QUE CREAR JOHAN CON EL EDITOR DE CODIGO Y EL TEST O GRAFICO*/}
     </div>
