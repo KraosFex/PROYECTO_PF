dc1d181ec3f5fdfa367c7f20d0670252dbf32aed
cesar <cesar.bfernandez@gmail.com>
2022-06-24 21:12:05 -0400

    add funtion isComplete and fixs

--- api-app/src/controllers/controllerLession.js
+++ api-app/src/controllers/controllerLession.js
@@ -4,11 +4,10 @@ const User = require('../model/modelUser')
 const ErrorResponse = require('../utils/errorResponse.js')
 
 const createLesson = async (req, res, next) => {
-  const { titulo, descripcion, num } = req.body
   try {
     const course = await Curso.findById(req.params.id)
     if (!course) return res.send({ info: 'El curso no existe' })
-    const newLesson = await Lesson.create({ titulo, descripcion, num })
+    const newLesson = await Lesson.create(req.body)
     const curso = await Curso.findByIdAndUpdate(req.params.id, {
       $push: { lessons: newLesson }
     })
@@ -35,13 +34,26 @@ const getLesson = async (req, res, next) => {
 const isCompleted = async (req, res) => {
   const id = req.user._id
   const idLesson = req.body
+  const idCourse = req.body
   try {
-    const user = await Course.findById(id)
-
+    const user = await User.findById(id)
+    const currentCourse = user.courses.filter(c => c._id == idCourse)
+    currentCourse.lessons[idLesson].isCompleted = true
+    const currentIndex = currentCourse.lessons.indexOf(idLesson)
+    if (currentIndex < currentCourse.lessons.length) {
+      const index = currentIndex + 1
+      currentCourse.lessons[index].isLocked = false
+    }
+    if (currentIndex === currentCourse.lessons.length) {
+      currentCourse.completed = true
+    }
+  } catch (err) {
+    res.status(500).send({ info: 'Error al obtener la consulta', err, success: false })
   }
 }
 
 module.exports = {
   createLesson,
-  getLesson
+  getLesson,
+  isCompleted
 }
