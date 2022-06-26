913a6b184437edea4c3601f0fe50825539d4e6c7
ChampiMagic <97173716+ChampiMagic@users.noreply.github.com>
2022-06-25 21:09:01 -0300

    Update user.js

--- api-app/src/routes/public/user.js
+++ api-app/src/routes/public/user.js
@@ -2,7 +2,7 @@ const { Router } = require('express')
 const router = Router()
 const { getUserById, topFive } = require('../../controllers/controllerUser.js')
 
-router.get('/topten', topFive)
+router.get('/topFive', topFive)
 router.get('/:id', getUserById)
 
 module.exports = router
