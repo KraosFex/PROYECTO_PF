2cda83bff983fbc1463ffdfcd9f792b22000a126
agustin panizza <agustin_panizza@hotmail.com>
2022-06-28 17:03:13 -0300

    redux toolkit

--- api-app/src/controllers/controllerLession.js
+++ api-app/src/controllers/controllerLession.js
@@ -5,22 +5,19 @@ const ErrorResponse = require('../utils/errorResponse.js')
 
 const createLesson = async (req, res, next) => {
   try {
-    const course = await Curso.findById(req.params.id)
-    if (!course) return res.send({ info: 'El curso no existe' })
-    const newLesson = await Lesson.create(req.body)
-    const curso = await Curso.findByIdAndUpdate(req.params.id, {
-      $push: {
-        lessons: {
-          lesson: newLesson
-        }
-      }
-    })
-    curso.lessons[0].isLocked = false
-    res.send({ info: 'Curso creado exitosamente', newLesson })
+    const course = await Curso.findById(req.params.id);
+    if (!course) return res.send({ info: "El curso no existe" });
+    const newLesson = await Lesson.create(req.body);
+    const updateCourse = await Curso.findByIdAndUpdate(req.params.id, {
+      $push: { lessons: newLesson },
+    });
+    updateCourse.lessons[0].lesson.isLocked = false;
+    await Curso.save();
+    res.send({ info: "Curso creado exitosamente", newLesson });
   } catch (err) {
-    next(new ErrorResponse(err, 500))
+    next(new ErrorResponse(err, 500));
   }
-}
+};
 
 const getLesson = async (req, res, next) => {
   const { id } = req.params;
@@ -37,27 +34,37 @@ const getLesson = async (req, res, next) => {
 const isCompleted = async (req, res) => {
   const id = req.user._id
   const { idLesson, idCourse } = req.body
+
   try {
     const user = await User.findById(id).populate({ path: 'courses.course', ref: 'Course', populate: { path: 'lessons.lesson', ref: 'Lesson' } })
     const currentCourse = user.courses.filter(c => c.course._id == idCourse)
-    const currentLesson = currentCourse[0].course.lessons.filter(l => l._id == idLesson)
-    currentLesson.set("isCompleted", true)
+    const currentLesson = currentCourse[0].course.lessons.filter(l => l.lesson._id == idLesson)
+    console.log("currentLesson", currentLesson)
+    currentLesson[0].lesson.set('isCompleted', true)
+    await user.save()
 
-    const currentIndex = currentCourse[0].course.lessons.findIndex(l => l._id == idLesson)
+    const currentIndex = currentCourse[0].course.lessons.findIndex(l => l.lesson._id == idLesson)
     const nextIndex = currentIndex + 1
-    if (currentIndex < currentCourse[0].course.lessons.length && currentIndex !== 0) {
-      currentCourse[0].course.lessons[nextIndex].set("isLocked", false)
+
+    if (currentCourse[0].course.lessons[nextIndex]) {
+      currentCourse[0].course.lessons[nextIndex].lesson.set('isLocked', false)
+      await user.save()
     }
 
-    if (currentIndex === 0 && currentCourse[0].course.lessons.length === 1) {
-      currentCourse[0].course.set("completed", true)
-    }
+    const lessonInFalse = currentCourse[0].course.lessons.filter(l => l.lesson.isCompleted === false)
 
-    //await user.save()
+    if (lessonInFalse.length) {
+      currentCourse[0].course.set('completed', false)
+      await user.save()
+    }
+    if (!lessonInFalse.length) {
+      currentCourse[0].course.set('completed', true)
+      await user.save()
+    }
 
     const updateUser = await User.findById(id).populate({ path: 'courses.course', ref: 'Course', populate: { path: 'lessons.lesson', ref: 'Lesson' } })
 
-    res.send({info: "curso completado", success: true, updateUser, nextLessonId: currentCourse[0].course.lessons[nextIndex]._id})
+    res.send({info: "clase completada", updateUser, success: true, nextLessonId: currentCourse[0].course.lessons[nextIndex].lesson._id  })
   } catch (err) {
     res
       .status(500)
--- api-app/src/middleware/protect.js
+++ api-app/src/middleware/protect.js
@@ -1,35 +1,29 @@
-const jwt = require("jsonwebtoken");
-const User = require("../model/modelUser.js");
-const ErrorResponse = require("../utils/errorResponse.js");
+const jwt = require('jsonwebtoken')
+const User = require('../model/modelUser.js')
+const ErrorResponse = require('../utils/errorResponse.js')
 
 const protect = async (req, res, next) => {
-  const { authorization } = req.headers;
+  const { authorization } = req.headers
 
-  let token;
+  let token
 
-  if (authorization && authorization.startsWith("Bearer")) {
-    token = authorization.split(" ")[1];
+  if (authorization && authorization.startsWith('Bearer')) {
+    token = authorization.split(' ')[1]
   }
 
-  if (!token)
-    return res
-      .status(401)
-      .send({ info: "No estas autorizado", success: false });
+  if (!token) return res.status(401).send({info: 'No estas autorizado', success: false})
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.id);
-    if (!user)
-      return res
-        .status(401)
-        .send({ info: "No estas autorizado", success: false });
-    req.user = user;
-    next();
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const user = await User.findById(decoded.id)
+    if (!user) return res.status(401).send({info: 'No estas autorizado', success: false})
+    req.user = user
+    next()
   } catch (err) {
-    res.status(500).send({ info: "Error al validar el token", success: false });
+    res.status(500).send({info: 'Error al validar el token', success: false})
   }
-};
+}
 
 module.exports = {
-  protect,
+  protect
 };
