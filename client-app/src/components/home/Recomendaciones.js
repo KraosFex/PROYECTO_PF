c504dd18a50d7febc8d83b2c2c7a1222f95b1628
Valentina <vcolmenares>
2022-06-30 00:15:20 -0300

    cosasHome

---
+++ client-app/src/components/home/Recomendaciones.js
@@ -0,0 +1,10 @@
+export var Reco = [
+    { name: "FLEXBOX FROGGY", hr: "http://flexboxfroggy.com/#es", etiqueta: "css" },
+    { name: "GRID GARDEN", hr: "https://cssgridgarden.com/#es", etiqueta: "css" },
+    { name: "Ruby Warrior", hr: "https://github.com/ryanb/ruby-warrior", etiqueta: "javascript" },
+    { name: "Flappy Bird", hr: "https://studio.code.org/flappy/1", etiqueta: "javascript" },
+    { name: "Code Combat", hr: "https://codecombat.com/", etiqueta: "html" },
+    { name: "CSS DINNER", hr: "https://flukeout.github.io/", etiqueta: "css" },
+    { name: "Robocode ", hr: "https://robocode.sourceforge.io/", etiqueta: "javascript" },
+    { name: "Elevator Saga", hr: "http://play.elevatorsaga.com/", etiqueta: "html" }
+]
